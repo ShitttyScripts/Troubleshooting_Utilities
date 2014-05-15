4a146a865cce3ee2f3d9ae72148b9093374a36b7
Wisconsam <sam@wisconsam.com>
2014-05-15 15:49:02 -0500

    adding scripts

---
+++ monitorProcesslist.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+COUNTER=0
+
+until [ $COUNTER -gt 9000 ];
+do
+	date >> /tmp/processlist.txt
+	/usr/local/mysql/bin/mysqladmin -u root processlist >> /tmp/processlist.txt
+	echo "" >> /tmp/processlist.txt
+        echo "" >> /tmp/processlist.txt
+	COUNTER=$[$COUNTER+1]
+	sleep 1
+done
